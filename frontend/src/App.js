fd88f981da1e3bc6bb79e239ef442fc1714beb47
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-06-02 21:25:45 +0700

    [FE][ManhLD]feat:login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from "./pages/login/login.tsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
