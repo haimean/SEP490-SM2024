4893a207fe01a19479479e345f24eb03668a8c57
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-06 19:29:17 +0700

    [BE][MinhPH]  feat api create, update, get all, get attribute Branches key with role admin

--- backend/src/modules/admin/admin.router.ts
+++ backend/src/modules/admin/admin.router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import attributeCourtRouter from './attributeCourt/attributeCourt.router';
+import attributeBranchesRouter from './attributeBranches/attributeBranches.router';
 
 const adminRouter = Router();
 adminRouter.use('/attribute-court', attributeCourtRouter);
+adminRouter.use('/attribute-branches', attributeBranchesRouter);
 
 export default adminRouter;
---
+++ backend/src/modules/admin/attributeBranches/attributeKeyBranches/attributeKeyBranches.router.ts
@@ -0,0 +1,41 @@
+import { Request, Response, Router } from 'express';
+import attributeKeyBranchesController from './attributeKeyBranches.controller';
+import attributeKeyBranchesValidator from './attributeKeyBranches.validator';
+import validate from '../../../../utils/validate';
+
+const attributeKeyBranchesRouter = Router();
+
+// create attribute key Branches
+attributeKeyBranchesRouter.post(
+  '/',
+  validate(attributeKeyBranchesValidator.create),
+  attributeKeyBranchesController.create
+);
+
+// update attribute key Branches
+attributeKeyBranchesRouter.put(
+  '/:id',
+  validate(attributeKeyBranchesValidator.update),
+  attributeKeyBranchesController.update
+);
+
+// TODO:
+// remove attribute key Branches
+attributeKeyBranchesRouter.delete(
+  '/:id',
+  attributeKeyBranchesController.remove
+);
+
+// get all attribute key Branches
+attributeKeyBranchesRouter.get(
+  '/',
+  attributeKeyBranchesController.getAll
+);
+
+// get attribute key Branches
+attributeKeyBranchesRouter.get(
+  '/:id',
+  attributeKeyBranchesController.get
+);
+
+export default attributeKeyBranchesRouter;
