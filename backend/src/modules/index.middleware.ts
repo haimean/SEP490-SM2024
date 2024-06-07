044124735edd1613d1b6d0cfef411ee66c17b7f4
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-08 03:01:59 +0700

    [BE][MinhPH]  update middelware prj

--- backend/src/modules/index.middleware.ts
+++ backend/src/modules/index.middleware.ts
@@ -2,6 +2,7 @@
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import CustomError from '../outcomes/customError';
 import { NextFunction, Request, Response } from 'express';
+import { Account } from '@prisma/client';
 
 const middlewares = {
   admin: (req: Request, res: Response, next: NextFunction) => {
@@ -9,19 +10,14 @@ const middlewares = {
       const secret: Secret = process.env.SECRET_JWT_KEY ?? '';
       // Lấy token từ request gửi đến
       const token = req.headers?.authorization?.split(' ')[1] ?? '';
-      const jwtObj: JwtPayload = jwt.verify(
-        token,
-        secret
-      ) as JwtPayload;
+      const jwtObj: Account = jwt.verify(token, secret) as Account;
       // Check role
-      // TODO: check role
-      // if (jwtObj.role !== 'admin') {
-      //   next(new CustomError('Authorization', 401));
-      // }
-
-      if (Date.now() > (jwtObj?.exp || 0) * 1000) {
-        next(new CustomError('Token is expired', 400));
+      if (jwtObj.role !== 'ADMIN') {
+        next(new CustomError('Authorization', 401));
       }
+      // if (Date.now() > (jwtObj?.exp || 0) * 1000) {
+      //   next(new CustomError('Token is expired', 400));
+      // }
       next();
     } catch (e: unknown) {
       if (typeof e === 'string') {
@@ -33,12 +29,48 @@ const middlewares = {
   },
 
   host: (req: Request, res: Response, next: NextFunction) => {
-    //TODO: middlewate for host
-    next();
+    try {
+      const secret: Secret = process.env.SECRET_JWT_KEY ?? '';
+      // Lấy token từ request gửi đến
+      const token = req.headers?.authorization?.split(' ')[1] ?? '';
+      const jwtObj: Account = jwt.verify(token, secret) as Account;
+      // Check role
+      if (jwtObj.role !== 'HOST') {
+        next(new CustomError('Authorization', 401));
+      }
+      // if (Date.now() > (jwtObj?.exp || 0) * 1000) {
+      //   next(new CustomError('Token is expired', 400));
+      // }
+      next();
+    } catch (e: unknown) {
+      if (typeof e === 'string') {
+        next(new CustomError(e.toUpperCase(), 500));
+      } else if (e instanceof Error) {
+        next(new CustomError(e.message, 500));
+      }
+    }
   },
   player: (req: Request, res: Response, next: NextFunction) => {
-    //TODO: middlewate for player
-    next();
+    try {
+      const secret: Secret = process.env.SECRET_JWT_KEY ?? '';
+      // Lấy token từ request gửi đến
+      const token = req.headers?.authorization?.split(' ')[1] ?? '';
+      const jwtObj: Account = jwt.verify(token, secret) as Account;
+      // Check role
+      if (jwtObj.role !== 'USER') {
+        next(new CustomError('Authorization', 401));
+      }
+      // if (Date.now() > (jwtObj?.exp || 0) * 1000) {
+      //   next(new CustomError('Token is expired', 400));
+      // }
+      next();
+    } catch (e: unknown) {
+      if (typeof e === 'string') {
+        next(new CustomError(e.toUpperCase(), 500));
+      } else if (e instanceof Error) {
+        next(new CustomError(e.message, 500));
+      }
+    }
   },
 };
 
