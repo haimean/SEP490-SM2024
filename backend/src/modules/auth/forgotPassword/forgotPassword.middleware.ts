0ac5283a62fcd25cf9aa26333ccbda79029bfdcb
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-08 03:02:42 +0700

    [BE][MinhPH]  update forgot pass middelware

--- backend/src/modules/auth/forgotPassword/forgotPassword.middleware.ts
+++ backend/src/modules/auth/forgotPassword/forgotPassword.middleware.ts
@@ -1,27 +1,8 @@
-import RandExp from 'randexp';
 import { Account } from '@prisma/client';
-import ejs from 'ejs';
 import { NextFunction, Request, Response } from 'express';
 import forgotPasswordService from './forgotPassword.service';
 import CustomError from '../../../outcomes/customError';
-import sendEmail from '../../../lib/sendEmail';
-import regex from '../../../utils/regex';
-import ResponseHandler from '../../../outcomes/responseHandler';
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import NotFoundError from '../../../outcomes/notFoundError';
 
-const getEmailComtent = (name: string, otp: string) => {
-  const templatePath = join(
-    __dirname,
-    '../../../mailTemplate/sendOtp.ejs'
-  );
-  const template = readFileSync(templatePath, 'utf8');
-  return ejs.render(template, {
-    name,
-    otp,
-  });
-};
 const forgotPasswordMiddleware = {
   findEmail: async (
     req: Request,
