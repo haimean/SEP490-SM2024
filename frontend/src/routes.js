151ab70a4e957ce1a6896c03a8ec3d9f88e1695e
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-10 01:44:10 +0700

    [BE][MinhPH]  update router

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,92 +1,92 @@
-import ForgotPassword from './pages/common/forgotPassword/ForgotPassword.js';
+import ForgotPassword from "./pages/common/forgotPassword/ForgotPassword.js";
 import Login from "./pages/common/login/Login.js";
-import SignUpFormPlayer from './pages/player/signUp/SignUp.js';
-import ListAttributeBranch from './pages/admin/AttributeBranch/ListAttributeBranch.js';
-import CreateAttributeBranch from './pages/admin/AttributeBranch/CreateAttributeBranch.js';
-import DetailAttributeBranch from './pages/admin/AttributeBranch/DetailAttributeBranch.js';
-import UpdateAttributeBranch from './pages/admin/AttributeBranch/UpdateAttributeBranch.js';
-import ListAttributeCourt from './pages/admin/AttributeCourt/ListAttributeCourt.js';
-import DetailAttributeCourt from './pages/admin/AttributeCourt/DetailAttributeCourt.js';
-import CreateAttributeCourt from './pages/admin/AttributeCourt/CreateAttributeCourt.js';
-import UpdateAttributeCourt from './pages/admin/AttributeCourt/UpdateAttributeCourt.js';
+import SignUpFormPlayer from "./pages/player/signUp/SignUp.js";
+import ListAttributeBranch from "./pages/admin/AttributeBranch/ListAttributeBranch.js";
+import CreateAttributeBranch from "./pages/admin/AttributeBranch/CreateAttributeBranch.js";
+import DetailAttributeBranch from "./pages/admin/AttributeBranch/DetailAttributeBranch.js";
+import UpdateAttributeBranch from "./pages/admin/AttributeBranch/UpdateAttributeBranch.js";
+import ListAttributeCourt from "./pages/admin/AttributeCourt/ListAttributeCourt.js";
+import DetailAttributeCourt from "./pages/admin/AttributeCourt/DetailAttributeCourt.js";
+import CreateAttributeCourt from "./pages/admin/AttributeCourt/CreateAttributeCourt.js";
+import UpdateAttributeCourt from "./pages/admin/AttributeCourt/UpdateAttributeCourt.js";
 
 const routes = [
   {
-    path: '/login',
-    name: 'Login',
-    component: <Login/>,
-    role: '',
-    layout: ''
+    path: "/login",
+    name: "Login",
+    component: <Login />,
+    role: "",
+    layout: "",
   },
   {
-    path: '/sign-up-player',
-    name: 'Sign Up Player',
-    component: <SignUpFormPlayer/>,
-    role: 'player',
-    layout: ''
+    path: "/sign-up-player",
+    name: "Sign Up Player",
+    component: <SignUpFormPlayer />,
+    role: "player",
+    layout: "",
   },
   {
-    path: '/forgot-password',
-    name: 'Forgot Password',
-    component: <ForgotPassword/>,
-    role: '',
-    layout: ''
+    path: "/forgot-password",
+    name: "Forgot Password",
+    component: <ForgotPassword />,
+    role: "",
+    layout: "",
   },
   {
-    path: '/branch-attribute',
-    name: 'Branch Attribute',
-    component: <ListAttributeBranch/>,
-    role: 'admin',
-    layout: ''
+    path: "/branch-attribute",
+    name: "Branch Attribute",
+    component: <ListAttributeBranch />,
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/detail-branch-attribute/:id',
-    name: 'Detail Branch Attribute',
+    path: "/detail-branch-attribute/:id",
+    name: "Detail Branch Attribute",
     component: <DetailAttributeBranch />,
-    role: 'admin',
-    layout: ''
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/create-branch-attribute',
-    name: 'Create Branch Attribute',
-    component: <CreateAttributeBranch/>,
-    role: 'admin',
-    layout: ''
+    path: "/create-branch-attribute",
+    name: "Create Branch Attribute",
+    component: <CreateAttributeBranch />,
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/update-branch-attribute',
-    name: 'Update Branch Attribute',
-    component: <UpdateAttributeBranch/>,
-    role: 'admin',
-    layout: ''
+    path: "/update-branch-attribute",
+    name: "Update Branch Attribute",
+    component: <UpdateAttributeBranch />,
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/court-attribute',
-    name: 'Court Attribute',
-    component: <ListAttributeCourt/>,
-    role: 'admin',
-    layout: ''
+    path: "/court-attribute",
+    name: "Court Attribute",
+    component: <ListAttributeCourt />,
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/detail-court-attribute/:id',
-    name: 'Detail Court Attribute',
+    path: "/detail-court-attribute/:id",
+    name: "Detail Court Attribute",
     component: <DetailAttributeCourt />,
-    role: 'admin',
-    layout: ''
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/create-court-attribute',
-    name: 'Create Court Attribute',
-    component: <CreateAttributeCourt/>,
-    role: 'admin',
-    layout: ''
+    path: "/create-court-attribute",
+    name: "Create Court Attribute",
+    component: <CreateAttributeCourt />,
+    role: "admin",
+    layout: "",
   },
   {
-    path: '/update-court-attribute',
-    name: 'Update Court Attribute',
-    component: <UpdateAttributeCourt/>,
-    role: 'admin',
-    layout: ''
+    path: "/update-court-attribute",
+    name: "Update Court Attribute",
+    component: <UpdateAttributeCourt />,
+    role: "admin",
+    layout: "",
   },
 ];
 
