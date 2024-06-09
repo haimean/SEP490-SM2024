2ac50fbb8feedf284d802c2d0531a9965001f584
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-06-09 22:41:08 +0700

    [FE][ManhLD]update:InputLabel

--- frontend/src/components/common/InputLabel.js
+++ frontend/src/components/common/InputLabel.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-const InputLabel = ({ label, id, register, errors, required }) => {
+const InputLabel = ({ label, id, register, errors, required, pattern, type = "text", minLength }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const inputType = type === "password" && showPassword ? "text" : type;
+
   return (
     <div className="mb-4">
       <label
@@ -9,14 +19,31 @@ const InputLabel = ({ label, id, register, errors, required }) => {
       >
         {label}
       </label>
-      <input
-        {...register(id, { required })}
-        id={id}
-        defaultValue=""
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      />
+      <div className="relative">
+        <input
+          {...register(id, { required, pattern, minLength })}
+          id={id}
+          defaultValue=""
+          type={inputType}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        {type === "password" && (
+          <span
+            onClick={togglePasswordVisibility}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 cursor-pointer"
+          >
+            {showPassword ? <RemoveRedEyeIcon/> : <VisibilityOffIcon/>}
+          </span>
+        )}
+      </div>
       {errors[id] && (
-        <p className="text-red-500 text-xs italic">{`${label} is required.`}</p>
+        <p className="text-red-500 text-xs italic">
+          {errors[id].type === "pattern"
+            ? pattern.message
+            : errors[id].type === "minLength"
+            ? `${label} must be at least ${minLength} characters.`
+            : `${label} is required.`}
+        </p>
       )}
     </div>
   );
