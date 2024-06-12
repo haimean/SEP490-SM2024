5edd35be54c7abcd7dfffd9288668ba24b36c9de
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-13 04:46:42 +0700

    [BE][MinhPH]  update middlware

--- backend/src/modules/index.middleware.ts
+++ backend/src/modules/index.middleware.ts
@@ -2,55 +2,53 @@
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import CustomError from '../outcomes/customError';
 import { NextFunction, Request, Response } from 'express';
-import { Account } from '@prisma/client';
+import { Account, Role } from '@prisma/client';
 import accountServiceBase from '../baseService/accountServiceBase';
 
-const middlewares = {
-  auth: (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const secret: Secret = process.env.SECRET_JWT_KEY ?? '';
-      // Lấy token từ request gửi đến
-      const token = req.headers?.authorization?.split(' ')[1] ?? '';
-      const jwtObj: Account = jwt.verify(token, secret) as Account;
-      if (jwtObj.id) {
-        const account = accountServiceBase.findById(jwtObj.id);
-        if (account) {
-          req.body.account = account;
-          next();
-        }
-      } else {
-        next(new CustomError('Authentication', 401));
-      }
-    } catch (e: unknown) {
-      if (typeof e === 'string') {
-        next(new CustomError(e.toUpperCase(), 500));
-      } else if (e instanceof Error) {
-        next(new CustomError(e.message, 500));
-      }
-    }
-  },
-  admin: (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.account.role !== 'ADMIN') {
-      next(new CustomError('Authorization', 401));
-    } else {
-      next();
-    }
-  },
+const secret: Secret = process.env.SECRET_JWT_KEY ?? '';
 
-  host: (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.account.role !== 'HOST') {
-      next(new CustomError('Authorization', 401));
-    } else {
-      next();
+const verifyToken = async (
+  req: Request,
+  next: NextFunction,
+  role?: string
+) => {
+  try {
+    const token = req.headers?.authorization?.split(' ')[1] ?? '';
+    const jwtObj: { data: Account } = jwt.verify(token, secret) as {
+      data: Account;
+    };
+
+    if (jwtObj.data.id) {
+      const account = await accountServiceBase.findById(
+        jwtObj.data.id
+      );
+      if (account) {
+        if (!role || account.role === role) {
+          return next();
+        } else {
+          return next(new CustomError('Authorization', 401));
+        }
+      }
     }
-  },
-  player: (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.account.role !== 'USER') {
-      next(new CustomError('Authorization', 401));
-    } else {
-      next();
+    return next(new CustomError('Authentication', 401));
+  } catch (e: unknown) {
+    if (typeof e === 'string') {
+      return next(new CustomError(e.toUpperCase(), 500));
+    } else if (e instanceof Error) {
+      return next(new CustomError(e.message, 500));
     }
-  },
+  }
+};
+
+const middlewares = {
+  auth: (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req, next),
+  admin: (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req, next, Role.ADMIN),
+  host: (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req, next, Role.HOST),
+  player: (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req, next, Role.USER),
 };
 
 export default middlewares;
