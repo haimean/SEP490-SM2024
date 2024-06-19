daedd6939d5017143d46f4eb015adf29de329dc0
inuyashapc162 <inuyashapc162@gmail.com>
2024-06-19 11:17:41 +0700

    [FE][AnhND] feat: list account

--- backend/src/modules/host/index.router.ts
+++ backend/src/modules/host/index.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
+import branchRouter from './branch/branch.router';
 
 const hostRouter = Router();
-
+hostRouter.use('/attribute-key-branches', branchRouter);
 export default hostRouter;
--- frontend/src/pages/test/Test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import DashboardLayout from "../../components/layout/dashboard";
-
-export default function Test() {
-  return <DashboardLayout>a</DashboardLayout>;
-}
