da039423b13505b2235cb33b03e506bf8e326f03
Pham Hai Minh <haiminh.work@gmail.com>
2024-06-30 15:12:31 +0700

    [BE][MinhPH]  feat api get and create attributebranches

---
+++ backend/src/modules/host/attributeBranches/attributeBranches.service.ts
@@ -0,0 +1,64 @@
+import {
+  AttributeBranches,
+  AttributeKeyBranches,
+} from '@prisma/client';
+import database from '../../../lib/db.server';
+
+const removeIdInObject = (data: any) => {
+  delete data.id;
+  delete data.createdAt;
+  delete data.updatedAt;
+  return data;
+};
+
+const attributeBranchesHostService = {
+  findAttributeKeyBranches: async (
+    id: number
+  ): Promise<AttributeKeyBranches | null> => {
+    return await database.attributeKeyBranches.findFirst({
+      where: {
+        id,
+        isActive: true,
+      },
+    });
+  },
+  create: async (payload: AttributeBranches): Promise<object> => {
+    return await database.attributeBranches.create({
+      data: removeIdInObject(payload),
+    });
+  },
+
+  getAll: async (accId: number): Promise<any> => {
+    return await database.attributeBranches.findMany({
+      where: {
+        isActive: true,
+        OR: [
+          { isPublic: true },
+          { isPublic: false, accountId: accId },
+        ],
+      },
+      include: {
+        account: true,
+        attributeKeyBranches: true,
+      },
+    });
+  },
+  get: async (id: number, accId: number): Promise<any> => {
+    return await database.attributeBranches.findFirst({
+      where: {
+        isActive: true,
+        id,
+        OR: [
+          { isPublic: true },
+          { isPublic: false, accountId: accId },
+        ],
+      },
+      include: {
+        account: true,
+        attributeKeyBranches: true,
+      },
+    });
+  },
+};
+
+export default attributeBranchesHostService;
