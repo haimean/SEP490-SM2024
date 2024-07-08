852ff1655c54acb22827e1af1d318908ab333f05
inuyashapc162 <inuyashapc162@gmail.com>
2024-07-08 23:03:43 +0700

    [FE][AnhND] refactor: router

--- frontend/src/pages/guest/Branch/BranchDetail.jsx
+++ frontend/src/pages/guest/Branch/BranchDetail.jsx
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../../layouts/player/Navbar";
 import Footer from "../../../layouts/player/Footer";
 import DetailPageCp from "../../../components/host/DetailPageCp";
