2ee05da7462f0ef4bd8b1dc8632a24c7827167bb
inuyashapc162 <inuyashapc162@gmail.com>
2024-07-16 13:15:33 +0700

    [BE][AnhND] refactor: getBookingHostList

--- backend/src/modules/host/booking/booking.service.ts
+++ backend/src/modules/host/booking/booking.service.ts
@@ -1,3 +1,4 @@
+import { includes } from '../../../../_templates/module/new/prompt';
 import database from '../../../lib/db.server';
 import { Pagination } from '../../index.model';
 import { getQueryPagination } from '../../index.service';
@@ -34,6 +35,42 @@ const bookingHostService = {
       },
     });
     const response = await database.booking.findMany(query);
+    return {
+      data: response,
+      total: result.length,
+    };
+  },
+  getBookingHostList: async (
+    accountId: number,
+    pagination: Pagination
+  ) => {
+    const query = {
+      where: {
+        accountId,
+        isDelete: false,
+        isAccept: true,
+      },
+      include: {},
+      ...getQueryPagination(pagination),
+      orderBy: {},
+    };
+    query.include = {
+      court: {
+        include: {
+          booking: {
+            include: {
+              account: {
+                include: {
+                  user: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const result = await database.branches.findMany(query);
+    const response = await database.branches.findMany(query);
     return {
       data: response,
       total: result.length,
