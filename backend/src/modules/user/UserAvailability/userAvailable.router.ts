e1639917cbbb57da55047feb044270dfa32c8b1e
Pham Hai Minh <haiminh.work@gmail.com>
2024-07-23 19:04:42 +0700

    [BE][MinhPH]  update bug

--- backend/src/modules/user/UserAvailability/userAvailable.router.ts
+++ backend/src/modules/user/UserAvailability/userAvailable.router.ts
@@ -3,4 +3,21 @@ import userAvailableController from './userAvailability.controller';
 
 const userAvailableRouter = Router();
 userAvailableRouter.post('/', userAvailableController.listAvailable);
+
+// những người khả dụng có thể tham gia trânj đấu
+userAvailableRouter.post(
+  '/:postId/get-user-free',
+  userAvailableController.getUserFree
+);
+
+userAvailableRouter.post(
+  '/:postId/get-user-match',
+  userAvailableController.getUserMatch
+);
+
+userAvailableRouter.post(
+  '/:postId/get-user-accept',
+  userAvailableController.getUserAccept
+);
+
 export default userAvailableRouter;
