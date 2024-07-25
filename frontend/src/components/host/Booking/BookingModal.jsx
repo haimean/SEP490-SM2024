e6c9cb44b8e8a65621c3ac22c0eebaf9abd79926
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-07-26 02:56:12 +0700

    [FE][ManhLD2nd]fix: Host set price

---
+++ frontend/src/components/host/Booking/BookingModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Modal, Box, Typography, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import CalendarModalComponen from './BookingCalendar';
+
+const BookingModal = ({ open, onClose, courtId }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 h-5/6 bg-white border border-gray-300 shadow-lg p-4 rounded-lg overflow-auto">
+        <IconButton
+          onClick={onClose}
+          className="!absolute !top-2 !right-2"
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Sân: {courtId}
+        </Typography>
+        <CalendarModalComponen courtId={courtId} />
+      </Box>
+    </Modal>
+  );
+};
+
+export default BookingModal;
