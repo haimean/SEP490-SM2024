15f1daf55aa7bc542c7912b4cc1882bf9457e5de
Pham Hai Minh <haiminh.work@gmail.com>
2024-07-28 17:20:50 +0700

    [BE][MinhPH]  fix bug xx

--- backend/src/lib/notificationService.ts
+++ backend/src/lib/notificationService.ts
@@ -1,15 +1,7 @@
 // src/notificationService.ts
-import { Notification, StatusNotification } from '@prisma/client';
+import { Notification } from '@prisma/client';
 import { databaseFirebase } from './firebaseConfig';
-import {
-  equalTo,
-  onValue,
-  orderByChild,
-  query,
-  ref,
-  remove,
-  set,
-} from 'firebase/database';
+import { ref, set } from 'firebase/database';
 import { randomUUID } from 'crypto';
 
 // Function to create notifications from an array
@@ -17,8 +9,7 @@ export const createNotifications = async (
   notifications: Notification[]
 ): Promise<void> => {
   for (let notification of notifications) {
-    const { accountId, message, url, createdAt }: Notification =
-      notification;
+    const { accountId, message, url }: Notification = notification;
     set(ref(databaseFirebase, 'users/' + randomUUID()), {
       accountId,
       message,
--- backend/src/modules/user/invitation/invitation.controller.ts
+++ backend/src/modules/user/invitation/invitation.controller.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import CustomError from '../../../outcomes/customError';
 import { ResponseHandler } from '../../../outcomes/responseHandler';
-import { Invitation, Post } from '@prisma/client';
+import { Invitation, UserAvailability } from '@prisma/client';
 import invitationUserService from './invitation.service';
+import { createNotifications } from '../../../lib/notificationService';
 
 const invitationUserController = {
+  // chủ trận mời người rảnh
   requestsToTheMatch: async (
     req: Request,
     res: Response,
@@ -18,6 +20,20 @@ const invitationUserController = {
           userAvailabilityId,
           postId
         );
+      const userAvailability: UserAvailability =
+        await invitationUserService.getUserAvailability(
+          userAvailabilityId
+        );
+      createNotifications([
+        {
+          accountId: userAvailability.accountId,
+          message: 'Có người mời bạn vào trận đấu của họ',
+          status: 'SEED',
+          url: '/',
+          createdAt: new Date(),
+          id: 23,
+        },
+      ]);
       ResponseHandler(res, invitation);
     } catch (error: any) {
       console.log(error.code);
