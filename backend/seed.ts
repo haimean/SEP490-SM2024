397a3276e5e5c6e418b9a34abdec74052e97246c
Pham Hai Minh <haiminh.work@gmail.com>
2024-07-28 23:06:59 +0700

    [BE][MinhPH]  fix bug xx

--- backend/seed.ts
+++ backend/seed.ts
@@ -10,36 +10,36 @@ const main = async () => {
   const seed = await createSeedClient();
 
   // Truncate all tables in the database
-
+  await seed.$resetDatabase();
   // await seed.account((x) =>
   //   x(10, () => ({
   //     user: (x) => x(1),
   //   }))
   // );
-  await seed.attributeKeyBranches((x) =>
-    x(5, () => ({
-      attributeBranches: (x) => x(10),
-    }))
-  );
-  await seed.attributeKeyCourt((x) => x(10));
-  await seed.attributeCourt((x) => x(50));
-  await seed.typeCourt((x) => x(10));
-  await seed.branches((x) =>
-    x(10, () => ({
-      address: (x) => x(1),
-    }))
-  );
-  await seed.court((x) => x(50));
-  await seed.blog((x) => x(10));
-  await seed.comment((x) => x(10));
-  await seed.priceTypeCourt((x) => x(10));
-  await seed.booking((x) => x(10));
-  await seed.bookingInfo((x) => x(10));
-  await seed.userAvailability((x) => x(10));
-  await seed.invitation((x) => x(10));
-  await seed.post((x) => x(10));
-  await seed.memberPost((x) => x(10));
-  await seed.review((x) => x(10));
+  // await seed.attributeKeyBranches((x) =>
+  //   x(5, () => ({
+  //     attributeBranches: (x) => x(10),
+  //   }))
+  // );
+  // await seed.attributeKeyCourt((x) => x(10));
+  // await seed.attributeCourt((x) => x(50));
+  // await seed.typeCourt((x) => x(10));
+  // await seed.branches((x) =>
+  //   x(10, () => ({
+  //     address: (x) => x(1),
+  //   }))
+  // );
+  // await seed.court((x) => x(50));
+  // await seed.blog((x) => x(10));
+  // await seed.comment((x) => x(10));
+  // await seed.priceTypeCourt((x) => x(10));
+  // await seed.booking((x) => x(10));
+  // await seed.bookingInfo((x) => x(10));
+  // await seed.userAvailability((x) => x(10));
+  // await seed.invitation((x) => x(10));
+  // await seed.post((x) => x(10));
+  // await seed.memberPost((x) => x(10));
+  // await seed.review((x) => x(10));
   // Type completion not working? You might want to reload your TypeScript Server to pick up the changes
 
   console.log('Database seeded successfully!');
