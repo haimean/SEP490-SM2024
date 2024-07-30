cf6cceae0bf555dedea5fdc01a5ad6ea1ebc87de
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-07-30 19:40:40 +0700

    [FE][ManhLD2nd]fix: Booking redirect

--- frontend/src/components/user/BookingTable/ConfirmBookingModal.jsx
+++ frontend/src/components/user/BookingTable/ConfirmBookingModal.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { Modal, Box, Typography, TextField, Button, CircularProgress, Backdrop } from '@mui/material';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 import CallApi from '../../../service/CallAPI';
+import RedirectConfirmModal from './RedirectConfirmModal';
 
 const ConfirmBookingModal = ({ isOpen, onRequestClose, courtId, selectedEvents, refreshData, resetEvents }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
+  const [isRedirectConfirmOpen, setIsRedirectConfirmOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   const pad = (n) => n.toString().padStart(2, '0');
 
@@ -54,6 +59,7 @@ const ConfirmBookingModal = ({ isOpen, onRequestClose, courtId, selectedEvents, 
       }, 10); 
       setName('');
       setPhone(''); 
+      setIsRedirectConfirmOpen(true);
     } catch (error) {
       toast.error(error.response?.data?.error || "An error occurred during booking");
     } finally {
@@ -61,52 +67,68 @@ const ConfirmBookingModal = ({ isOpen, onRequestClose, courtId, selectedEvents, 
     }
   };
 
+  const handleCloseRedirectConfirm = () => {
+    setIsRedirectConfirmOpen(false);
+  };
+
+  const handleConfirmRedirect = () => {
+    setIsRedirectConfirmOpen(false);
+    navigate('/player/booking-history');  // Thay '/your-target-url' bằng URL thực tế của bạn
+  };
+
   return (
-    <Modal open={isOpen} onClose={!loading ? onRequestClose : null} aria-labelledby="booking-modal-title" aria-describedby="booking-modal-description">
-      <Box sx={{ bgcolor: 'background.paper', p: 4, borderRadius: 1, maxWidth: 500, mx: 'auto', mt: 10, position: 'relative' }}>
-        <Backdrop
-          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={loading}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-        <Typography id="booking-modal-title" variant="h6" component="h2" className="!mb-4">
-          Nhập thông tin người đặt
-        </Typography>
-        <TextField
-          label="Tên"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          fullWidth
-          className="!mb-4"
-        />
-        <TextField
-          label="Số điện thoại"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          fullWidth
-          className="!mb-4"
-        />
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
-          <Button
-            onClick={handleConfirmBooking}
-            variant="contained"
-            color="primary"
-            disabled={loading} 
+    <>
+      <Modal open={isOpen} onClose={!loading ? onRequestClose : null} aria-labelledby="booking-modal-title" aria-describedby="booking-modal-description">
+        <Box sx={{ bgcolor: 'background.paper', p: 4, borderRadius: 1, maxWidth: 500, mx: 'auto', mt: 10, position: 'relative' }}>
+          <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={loading}
           >
-            Xác nhận
-          </Button>
-          <Button
-            onClick={onRequestClose}
-            variant="outlined"
-            color="primary"
-            disabled={loading} 
-          >
-            Đóng
-          </Button>
+            <CircularProgress color="inherit" />
+          </Backdrop>
+          <Typography id="booking-modal-title" variant="h6" component="h2" className="!mb-4">
+            Nhập thông tin người đặt
+          </Typography>
+          <TextField
+            label="Tên"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            fullWidth
+            className="!mb-4"
+          />
+          <TextField
+            label="Số điện thoại"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            fullWidth
+            className="!mb-4"
+          />
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
+            <Button
+              onClick={handleConfirmBooking}
+              variant="contained"
+              color="primary"
+              disabled={loading} 
+            >
+              Xác nhận
+            </Button>
+            <Button
+              onClick={onRequestClose}
+              variant="outlined"
+              color="primary"
+              disabled={loading} 
+            >
+              Đóng
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Modal>
+      </Modal>
+      <RedirectConfirmModal
+        isOpen={isRedirectConfirmOpen}
+        onClose={handleCloseRedirectConfirm}
+        onConfirm={handleConfirmRedirect}
+      />
+    </>
   );
 };
 
--- frontend/src/components/user/Court/CourtCard.jsx
+++ frontend/src/components/user/Court/CourtCard.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { Card, CardContent, CardMedia, Typography, Stack, Button, Tooltip } from "@mui/material";
 import ChecklistIcon from '@mui/icons-material/Checklist';
 import BookingModal from "../BookingTable/BookingModal";
+import { Link} from "react-router-dom";
 
 const CourtCard = ({ court, image }) => {
     const [openModal, setOpenModal] = useState(false);
-
     const handleBookClick = (event) => {
         event.stopPropagation(); // Prevent the card click event
         setOpenModal(true);
@@ -14,13 +14,8 @@ const CourtCard = ({ court, image }) => {
     const handleCloseModal = () => {
         setOpenModal(false);
     };
-
-    const handleCardClick = () => {
-        window.open(`/branch/4/court/${court.id}`, '_blank');
-    };
-
     return (
-        <Card onClick={handleCardClick} style={{ cursor: 'pointer' }}>
+        <Card >
             <CardMedia
                 component="img"
                 image={image}
@@ -45,6 +40,14 @@ const CourtCard = ({ court, image }) => {
                         onClick={handleBookClick}
                     >
                         Đặt sân
+                    </Button>
+                    <Button
+                        variant="contained"
+                        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+                        component={Link}
+                        to={`/branch/4/court/${court.id}`}
+                    >
+                        Xem chi tiết
                     </Button>
                 </div>
             </CardContent>
