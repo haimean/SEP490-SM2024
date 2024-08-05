75f8f7f76daf5162bf1bdcb4331fc6b5c6513e2d
Nguyen Toan Thang <thangnthe163199@fpt.edu.vn>
2024-08-05 16:02:24 +0700

    [FE][ThangNT] fix: xoa icon flag

--- frontend/src/components/player/Blog/BlogDetailModal.jsx
+++ frontend/src/components/player/Blog/BlogDetailModal.jsx
@@ -10,8 +10,7 @@ import {
   Divider,
   DialogActions,
 } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import FlagIcon from "@mui/icons-material/Flag";
+import {Close} from "@mui/icons-material";
 import { getTimeSinceCreation } from "../../../utils/getTimeSinceCreation";
 import ViewCommentByBlog from "../../../components/player/Blog/ViewCommentByBlog";
 import CreateComment from "./CreateComment";
@@ -40,7 +39,7 @@ const BlogDetailModal = ({ open, onClose, blog }) => {
             Bài viết của {blog?.account?.user?.fullName}
           </Typography>
           <IconButton onClick={onClose}>
-            <CloseIcon />
+            <Close />
           </IconButton>
         </Box>
       </DialogTitle>
@@ -64,11 +63,6 @@ const BlogDetailModal = ({ open, onClose, blog }) => {
               </Typography>
             </Box>
           </Box>
-          {currentAccountId !== blog?.accountId && (
-            <IconButton aria-label="report" onClick={handleReportClick}>
-              <Flag />
-            </IconButton>
-          )}
         </Box>
         <Typography variant="body1" color="text.secondary">
           {blog?.caption?.split("\n").map((line, index) => (
