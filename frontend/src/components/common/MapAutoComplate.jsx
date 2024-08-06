7ce59c1daadb2ce7657795c04ea8c2ecebaf1ba1
inuyashapc162 <inuyashapc162@gmail.com>
2024-08-06 19:45:56 +0700

    [FE][AnhND] refactor: create branch

--- frontend/src/components/common/MapAutoComplate.jsx
+++ frontend/src/components/common/MapAutoComplate.jsx
@@ -135,7 +135,7 @@ const MapComponent = ({ onSubmit }) => {
           <Popup>{address}</Popup>
         </Marker>
       </MapContainer>
-      <div style={{ marginTop: "10px" }}>
+      {/* <div style={{ marginTop: "10px" }}>
         <h3>Location Details</h3>
         <p>
           <strong>Latitude:</strong> {details.latitude}
@@ -146,7 +146,7 @@ const MapComponent = ({ onSubmit }) => {
         <p>
           <strong>Address:</strong> {JSON.stringify(details?.address)}
         </p>
-      </div>
+      </div> */}
     </Box>
   );
 };
---
+++ frontend/src/components/host/FormInput/Editor.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import { Editor } from "primereact/editor";
+export default function EditorInput({ field, control, errors }) {
+  return (
+    <Controller
+      name={field.name}
+      control={control}
+      defaultValue={field.defaultValue || ""}
+      errors={errors}
+      rules={{
+        required: field.required ? `${field.label} là bắt buộc` : false,
+        validate: (value) =>
+          value.trim() !== "" ||
+          `${field.label} không thể chỉ chứa khoảng trắng`,
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <div>
+          <Editor
+            className="w-full"
+            onTextChange={(e) => onChange(e.htmlValue)}
+            value={value}
+            error={!!error}
+            style={{ height: "320px" }}
+            required={field.required}
+            helperText={error ? error.message : null}
+          />
+          {error && <span className="text-red-500">{`${error.message}`}</span>}
+        </div>
+      )}
+    />
+  );
+}
