cd3900d0de9a6cd12a38e09a99ff0987d6c5a973
Pham Hai Minh <haiminh.work@gmail.com>
2024-08-13 02:01:04 +0700

    [FE][MinhPH]  fix defect 82

--- frontend/src/pages/guest/Court/ListCourt.jsx
+++ frontend/src/pages/guest/Court/ListCourt.jsx
@@ -13,17 +13,6 @@ const ListCourt = () => {
   console.log("🚀 ========= storedUserRole:", storedUserRole);
   const { id } = useParams();
   const { openDialog, DialogComponent } = useDialogConfirm();
-  const [courtList, setCourtList] = useState([
-    {
-      id: 1,
-      court: null,
-    },
-    {
-      id: 2,
-      court: null,
-    },
-  ]);
-  const [isCompare, setIsCompare] = useState(false);
   const [data, setData] = useState([]);
   const [registerCourtModal, setRegisterCourtModal] = useState(false);
   const handleOpenModalRegisterCourt = () => {
@@ -32,31 +21,6 @@ const ListCourt = () => {
   const handleCloseModalRegisterCourt = async () => {
     setRegisterCourtModal(false);
     await getAllCourt();
-  };
-  const handleCompare = (court) => {
-    setIsCompare(true);
-    setCourtList((prev) => {
-      if (prev[0].court == null) {
-        return [
-          {
-            ...prev[0],
-            court: court, // Assuming 'court' is the value you want to assign
-          },
-          prev[1], // Keep the second item unchanged
-        ];
-      } else if (prev[0].court != null && prev[1].court == null) {
-        return [
-          prev[0], // Keep the first item unchanged
-          {
-            ...prev[1],
-            court: court, // Assuming 'court' is the value you want to assign
-          },
-        ];
-      }
-
-      // Return prev as is if neither condition is met
-      return prev;
-    });
   };
   const getAllCourt = async () => {
     try {
@@ -69,16 +33,8 @@ const ListCourt = () => {
   useEffect(() => {
     getAllCourt();
   }, []);
-  const handleRemoveCompare = (court) => {
-    console.log("🚀 ========= court:", court);
-    setCourtList((prev) =>
-      prev.map((item) =>
-        item.id === court.id ? { ...item, court: null } : item
-      )
-    );
-  };
-  const handleDeleteCourt = async (id) => {
-    openDialog(`Bạn có muốn xóa sân ${id} không ?`, async () => {
+  const handleDeleteCourt = async (id, name) => {
+    openDialog(`Bạn có muốn xóa sân ${name} không ?`, async () => {
       try {
         const result = await CallApi(
           `/api/host/court/delete-court/${id}`,
@@ -117,11 +73,6 @@ const ListCourt = () => {
           <CourtDetailList
             key={index}
             activity={activity}
-            courtList={courtList}
-            handleCompare={handleCompare}
-            isCompare={isCompare}
-            setIsCompare={setIsCompare}
-            handleRemoveCompare={handleRemoveCompare}
             onDeleteCourt={handleDeleteCourt}
             role={storedUserRole}
             branchId={id}
