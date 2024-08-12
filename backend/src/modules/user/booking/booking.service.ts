5d993826e0986bbb8855e33808fcc0356b7b0eda
Pham Hai Minh <haiminh.work@gmail.com>
2024-08-12 22:16:36 +0700

    [FE][MinhPH]  fix bug 132

--- backend/src/modules/user/booking/booking.service.ts
+++ backend/src/modules/user/booking/booking.service.ts
@@ -143,6 +143,42 @@ const bookingUserService = {
       },
     });
   },
+  // check Booking có trùng lịch không
+  checkBookingConflict: async (
+    accountId: number,
+    startTime: Date,
+    endTime: Date
+  ) => {
+    const conflictingBooking = await database.booking.findFirst({
+      where: {
+        isDelete: false,
+        OR: [
+          {
+            accountId: accountId,
+            startTime: {
+              lte: endTime,
+            },
+            endTime: {
+              gte: startTime,
+            },
+          },
+          {
+            post: {
+              invitation: {
+                every: {
+                  status: 'ACCEPT',
+                  userAvailability: {
+                    accountId: accountId,
+                  },
+                },
+              },
+            },
+          },
+        ],
+      },
+    });
+    return conflictingBooking !== null;
+  },
 };
 
 export default bookingUserService;
