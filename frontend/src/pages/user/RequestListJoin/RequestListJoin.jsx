ba44bc909ac1fb805601e7637bec7a1613f7bc18
inuyashapc162 <inuyashapc162@gmail.com>
2024-08-13 15:47:19 +0700

    [FE][AnhND] feat: tab lịch sử đặt sân

--- frontend/src/pages/user/RequestListJoin/RequestListJoin.jsx
+++ frontend/src/pages/user/RequestListJoin/RequestListJoin.jsx
@@ -10,12 +10,14 @@ import { Link } from "react-router-dom";
 import FormatTime from "../../../utils/user/formatTime";
 import haversine from "haversine";
 import useDialogConfirm from "../../../hooks/useDialogConfirm";
+import RequestListJoinCp from "../../../components/user/RequestListJoin/RequestListJoinCp";
 
 export default function RequestListJoin() {
   const [requestList, setRequestList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
-
   const [location, setLocation] = React.useState(null);
+  const [value, setValue] = React.useState(0);
+
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -39,7 +41,9 @@ export default function RequestListJoin() {
   const getRequestList = async () => {
     setIsLoading(true);
     try {
-      const data = await CallApi(api, "get");
+      const data = await CallApi(api, "post", {
+        status: value == 0 ? "NEW" : value == 1 ? "ACCEPT" : "NOACCEPT",
+      });
       console.log("🚀 ========= data:", data);
       setRequestList(data.data);
       setIsLoading(false);
@@ -68,9 +72,11 @@ export default function RequestListJoin() {
       console.log("🚀 ========= error:", error);
     }
   };
+
   React.useEffect(() => {
     getRequestList();
-  }, []);
+  }, [value]);
+
   const distance = (latitude, longitude) => {
     return haversine(
       {
@@ -90,7 +96,6 @@ export default function RequestListJoin() {
   React.useEffect(() => {
     getLocation();
   }, []);
-  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -134,130 +139,28 @@ export default function RequestListJoin() {
           </Box>
           {value == 0 &&
             requestList?.map((item) => (
-              <Grid key={item?.id} item xs={12}>
-                <Card variant="outlined" className="p-4 pb-2">
-                  <Grid container spacing={1}>
-                    <Grid item xs={8}>
-                      <Link
-                        to={`/post/${item?.Post?.id}`}
-                        className="hover:underline hover:cursor-pointer"
-                      >
-                        <Typography variant="subtitle1" color="primary">
-                          {item?.Post?.title}
-                          {/* Tên bài post: {item?.Post?.booking?.bookingInfo?.name} */}
-                        </Typography>
-                      </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                      {item?.status == "NEW" ? (
-                        <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-                          Đang yêu cầu vào trận
-                        </span>
-                      ) : (
-                        <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                          Đã tham gia
-                        </span>
-                      )}
-                    </Grid>
-                  </Grid>
-
-                  <Grid container>
-                    <Grid item xs={6}>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        Vị trí cách bạn:{" "}
-                        {distance(
-                          item?.Post?.booking?.Court?.Branches?.address
-                            ?.latitude,
-                          item?.Post?.booking?.Court?.Branches?.address
-                            ?.longitude
-                        ) != null
-                          ? distance(
-                              item?.Post?.booking?.Court?.Branches?.address
-                                ?.latitude,
-                              item?.Post?.booking?.Court?.Branches?.address
-                                ?.longitude
-                            ).toFixed(2)
-                          : "~"}{" "}
-                        km
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        {`Thời gian xin vào trận: ${getTimeSinceCreation(
-                          item?.updatedAt
-                        )}`}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        Liên hệ: {item?.Post?.booking?.bookingInfo?.numberPhone}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        {`Giờ chơi: ${FormatTime(
-                          item?.Post?.booking?.startTime
-                        )} - ${FormatTime(item?.Post?.booking?.endTime)}`}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        {`Địa chỉ: ${item?.Post?.booking?.Court?.Branches?.address?.detail}`}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                        className="flex justify-end"
-                      >
-                        <Button
-                          onClick={() =>
-                            changeStatusInvitation(
-                              item?.id,
-                              "CANCEL",
-                              "Hủy lời mời"
-                            )
-                          }
-                          color="error"
-                          variant="contained"
-                          size="small"
-                        >
-                          Hủy yêu cầu
-                        </Button>
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Card>
-              </Grid>
+              <RequestListJoinCp
+                key={item?.id}
+                item={item}
+                changeStatusInvitation={changeStatusInvitation}
+              />
             ))}
-
+          {value == 1 &&
+            requestList?.map((item) => (
+              <RequestListJoinCp
+                key={item?.id}
+                item={item}
+                changeStatusInvitation={changeStatusInvitation}
+              />
+            ))}
+          {value == 2 &&
+            requestList?.map((item) => (
+              <RequestListJoinCp
+                key={item?.id}
+                item={item}
+                changeStatusInvitation={changeStatusInvitation}
+              />
+            ))}
           <DialogComponent />
         </Grid>
       )}
