300a477ff39eb3828b4a2bffe1d53da9947854ab
Pham Hai Minh <haiminh.work@gmail.com>
2024-08-24 00:54:21 +0700

    [FE][MinhPH]  fix bug

--- backend/src/lib/upLoadImageService.ts
+++ backend/src/lib/upLoadImageService.ts
@@ -1,7 +1,5 @@
 import { UploadApiResponse } from 'cloudinary';
-import crypto from 'crypto';
 import dotenv from 'dotenv';
-import { v4 as uuidv4 } from 'uuid';
 
 import { logger } from '../utils/logger';
 import cloudinary from './cloudinary';
@@ -12,12 +10,14 @@ export async function uploadFile(
   file: Express.Multer.File
 ): Promise<string> {
   try {
+    console.log(new Date().getTime().toString());
+
     const result = await new Promise<UploadApiResponse>(
       (resolve, reject) => {
         cloudinary.uploader
           .upload_stream(
             {
-              folder: uuidv4(), // Thay 'your_folder_name' bằng tên thư mục bạn muốn lưu trữ trên Cloudinary
+              folder: new Date().getTime().toString(), // Thay 'your_folder_name' bằng tên thư mục bạn muốn lưu trữ trên Cloudinary
               public_id:
                 file.filename + new Date().getTime().toString(), // Đặt tên file tùy chỉnh
               overwrite: true, // Tùy chọn để ghi đè nếu file có cùng tên đã tồn tại
