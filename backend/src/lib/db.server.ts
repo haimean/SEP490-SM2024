62add3c159266b87be9c770fc6b3a522d32b4524
Pham Hai Minh <haiminh.work@gmail.com>
2024-08-24 14:59:22 +0700

    [FE][MinhPH]  fix bug

--- backend/src/lib/db.server.ts
+++ backend/src/lib/db.server.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import dateTimeMiddleware from '../utils/prisma-middleware';
 
 const database: PrismaClient = new PrismaClient();
-database.$use(dateTimeMiddleware);
 export default database;
