c035b5a97fafc26e3208700eb10eb8422b37bbe1
inuyashapc162 <inuyashapc162@gmail.com>
2024-08-24 21:11:46 +0700

    [FE][AnhND] refactor: loading

--- frontend/src/pages/host/Court/CourtDetailHost.jsx
+++ frontend/src/pages/host/Court/CourtDetailHost.jsx
@@ -3,12 +3,11 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import CallApi from "../../../service/CallAPI";
 import CourtDetailComponent from "../../../components/host/court/CourtDetailComponent";
-import Footer from "../../../layouts/player/Footer";
-import Navbar from "../../../layouts/player/NavbarUser";
 import PropTypes from "prop-types";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import { useParams } from "react-router-dom";
+import Loading from "../../../components/common/Loading";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -48,6 +47,8 @@ export default function CourtDetailHost() {
   const { idCourt } = useParams();
   const [id, setId] = React.useState(idCourt);
   const [reload, setReload] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(false);
+
   const { idBranch } = useParams();
 
   const userRole = localStorage.getItem("userRole") || "";
@@ -56,6 +57,7 @@ export default function CourtDetailHost() {
     setValue(newValue);
   };
   const getAllCourtByBranch = async () => {
+    setIsLoading(true);
     try {
       const result = await CallApi(`/api/court/branch/${idBranch}`, "get");
       const resultCourt = await CallApi(
@@ -65,6 +67,7 @@ export default function CourtDetailHost() {
       // console.log("🚀 ========= result:", result.data);
       setCourt(result.data);
       setData(resultCourt.data);
+      setIsLoading(false);
     } catch (error) {
       console.log("🚀 ========= error:", error);
     }
@@ -87,56 +90,56 @@ export default function CourtDetailHost() {
   React.useEffect(() => {
     getCourtDetail();
   }, [reload]);
-  return (
-    <>
-      <div className="bg-gray-100 min-h-screen p-4">
-        <div className="container mx-auto p-4 mt-16">
-          <Box sx={{ width: "100%" }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-              variant="scrollable"
-              scrollButtons="auto"
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <div className="bg-gray-100 min-h-screen p-4">
+      <div className="container mx-auto p-4 mt-16">
+        <Box sx={{ width: "100%" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            {court &&
+              court?.map((item, index) => (
+                <Tab
+                  key={item.id}
+                  onClick={() => {
+                    setId(item.id);
+                    setReload(index);
+                  }}
+                  label={item.name}
+                  {...a11yProps(index)}
+                />
+              ))}
+          </Tabs>
+          <CustomTabPanel value={value} index={reload}>
+            <Box
+              sx={{
+                flexGrow: 1,
+              }}
             >
-              {court &&
-                court?.map((item, index) => (
-                  <Tab
-                    key={item.id}
-                    onClick={() => {
-                      setId(item.id);
-                      setReload(index);
-                    }}
-                    label={item.name}
-                    {...a11yProps(index)}
-                  />
-                ))}
-            </Tabs>
-            <CustomTabPanel value={value} index={reload}>
-              <Box
-                sx={{
-                  flexGrow: 1,
-                }}
-              >
-                {data && (
-                  <CourtDetailComponent
-                    title={data?.name}
-                    image={data?.Branches?.image}
-                    location={data?.Branches?.addressLatitude}
-                    date={data?.createdAt}
-                    description={data?.TypeCourt?.description}
-                    participants={data?.Branches?.name}
-                    type={"courtDetail"}
-                    role={userRole}
-                    court={data}
-                    courtId={id}
-                  />
-                )}
-              </Box>
-            </CustomTabPanel>
-          </Box>
-        </div>
+              {data && (
+                <CourtDetailComponent
+                  title={data?.name}
+                  image={data?.Branches?.image}
+                  location={data?.Branches?.addressLatitude}
+                  date={data?.createdAt}
+                  description={data?.TypeCourt?.description}
+                  participants={data?.Branches?.name}
+                  type={"courtDetail"}
+                  role={userRole}
+                  court={data}
+                  courtId={id}
+                />
+              )}
+            </Box>
+          </CustomTabPanel>
+        </Box>
       </div>
-    </>
+    </div>
   );
 }
